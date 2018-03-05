f7f89af948ab70edd78639870b65d0158141d4bc
Bartek <bartek_borzyszkowski@wp.pl>
2018-03-05 17:48:16 +0100

    step 1

--- Mathematical Modelling/Mathematical_Modelling.py
+++ Mathematical Modelling/Mathematical_Modelling.py
@@ -1,11 +1,29 @@
+import numpy as np
+import matplotlib as plt
+
 def topic ():
     print("Metody Modelowania Matematycznego - Projekt")
     print("Temat nr 7")
-    print("Bartłomiej Borzyszkowski")
+    print("Bartlomiej Borzyszkowski")
     print("Jan Michalik", '\n')
+
+def userdefine ():
+    print ("Podaj dodatnie wartosci dla opornikow i kondensatorow")
+    R1,R2,C1,C2=-1,-1,-1,-1
+    while(R1<0):
+        R1=float(input("R1: "))
+    while(R2<0):
+        R2=float(input("R2: "))
+    while(C1<0):
+        C1=float(input("C1: "))
+    while (C2<0):
+        C2=float(input("C2: "))
+    return  R1, R2, C1, C2
 
 def main ():
     topic()
+    R1,R2,C1,C2 = userdefine()
+    print(R1)
 
 main()
 
