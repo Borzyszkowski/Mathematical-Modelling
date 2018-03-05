69d0afcdc3d4ea10767d2d94e16bc29b945f5cfb
Bartek <bartek_borzyszkowski@wp.pl>
2018-03-05 16:57:35 +0100

    Start

---
+++ Mathematical Modelling/Mathematical_Modelling.py
@@ -0,0 +1,11 @@
+def topic ():
+    print("Metody Modelowania Matematycznego - Projekt")
+    print("Temat nr 7")
+    print("Bartłomiej Borzyszkowski")
+    print("Jan Michalik", '\n')
+
+def main ():
+    topic()
+
+main()
+
